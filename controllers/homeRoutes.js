16010faea1a06cf07f47dde0b8d5238b4ba3e9ce
lkrum <leahkrumholz73@gmail.com>
2023-05-13 22:57:54 -0500

    got conditionals to run on homepage, need help with dashboard conditionals

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,24 @@
+// WHEN I click on an existing blog post
+// THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
+// WHEN I enter a comment and click on the submit button while signed in
+//   THEN the comment is saved and the post is updated to display the comment, the comment creator’s username, and the date created
+// WHEN I click on the dashboard option in the navigation
+// THEN I am taken to the dashboard and presented with any blog posts I have already created and the option to add a new blog post
+// WHEN I click on the button to add a new blog post
+// THEN I am prompted to enter both a title and contents for my blog post
+// WHEN I click on the button to create a new blog post
+// THEN the title and contents of my post are saved and I am taken back to an updated dashboard with my new blog post
+// WHEN I click on one of my existing posts in the dashboard
+// THEN I am able to delete or update my post and taken back to an updated dashboard
+// WHEN I click on the logout option in the navigation
+// THEN I am signed out of the site
+// WHEN I am idle on the site for more than a set time
+// THEN I am able to view posts and comments but I am prompted to log in again before I can add, update, or delete posts
+
+
 const router = require('express').Router();
 const { Post, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
@@ -54,8 +72,7 @@ router.get('/posts/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-//  withAuth
-router.get('/posts', async (req, res) => {
+router.get('/posts', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -65,7 +82,7 @@ router.get('/posts', async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render('dashboard', {
       ...user,
       logged_in: true
     });
@@ -77,7 +94,7 @@ router.get('/posts', async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
 
